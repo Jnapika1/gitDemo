6d450414011b870580d93f3d3c4358ae58cb2a03
Jnapika1 <jnapika.aish@gmail.com>
2023-04-24 22:00:21 +0530

    added changes to list items

--- dom.js
+++ dom.js
@@ -36,3 +36,14 @@ header.style.borderBottom='solid 3px black';
 
 addItems.style.fontWeight = 'bold';
 addItems.style.color='green';
+
+let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='Hello 2';
+// items[1].style.fontWeight='bold';
+items[2].style.backgroundColor='green';
+
+for(let i=0; i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
