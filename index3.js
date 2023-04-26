41bb47a711d4f5b7bc6e98458007c1557e72535b
Jnapika1 <jnapika.aish@gmail.com>
2023-04-26 23:06:22 +0530

    added delete from ls functionality

--- index3.js
+++ index3.js
@@ -2,8 +2,7 @@ const myForm = document.querySelector('#form1');
 const mySub = document.querySelector('#submit');
 const nameInput = document.querySelector('#fname');
 const emailInput = document.querySelector('#email');
-
-
+const ul=document.getElementById("list-group");
 
 
 
@@ -21,5 +20,28 @@ function onSubmit(e){
         }
         let obj = JSON.stringify(myObj);
         localStorage.setItem(nameInput.value, obj);
+        showObjOnScreen(myObj);
+    }
+}
+function showObjOnScreen(obj){
+    
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${obj.name}`));
+    li.appendChild(document.createTextNode(` - ${obj.email}`));
+    ul.appendChild(li);
+
+    let del = document.createElement('button');
+    del.className="btn btn-danger btn-sm float-right delete";
+    del.appendChild(document.createTextNode("delete"));
+    li.appendChild(del);
+}
+
+ul.addEventListener('click', deleteItem);
+function deleteItem(e){
+    if(e.target.classList.contains('delete')){
+        let user = e.target.parentElement;
+        ul.removeChild(user);
+        console.log(user.firstChild.textContent);
+        localStorage.removeItem(user.firstChild.textContent);
     }
 }
